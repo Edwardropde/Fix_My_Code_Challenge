77a31aca8c5c9ea77d8730c720de3532b7e88f73
Edwardropde <edwardropha@gmail.com>
2023-12-19 11:59:43 +0000

    correction

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,7 +3,7 @@
 
 
 class Square():
-    """ Class Docs"""
+    """Class Docs"""
     width = 0
     height = 0
 
@@ -26,7 +26,7 @@ class Square():
 
 
 if __name__ == "__main__":
-
+    """ Final DOcs """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
